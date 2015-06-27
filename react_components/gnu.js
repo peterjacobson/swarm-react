4bad92755b14d66c335f52474b3664c043186dad
peterjacobson <peterjacobson.nz@gmail.com>
2015-06-27 22:02:08 +1200

    split gnu component

---
+++ react_components/gnu.js
@@ -0,0 +1,13 @@
+var React = require('react')
+
+var imgStyle = {
+	width: '30%'
+};
+
+module.exports = React.createClass({
+	render: function () {
+		return (
+			<img style={imgStyle} src="img/691px-Gnu_meditate_levitate.png"></img>
+		)
+	}
+})
