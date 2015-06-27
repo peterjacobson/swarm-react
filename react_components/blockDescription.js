c566d52c76b64743e68942d89ff47b6d6fe90b25
peterjacobson <peterjacobson.nz@gmail.com>
2015-06-27 18:13:07 +1200

    add page icon

--- react_components/blockDescription.js
+++ react_components/blockDescription.js
@@ -2,15 +2,29 @@ var React = require('react')
 var imgStyle = {
 	width: '30%'
 };
+var textareaStyle = {
+	border: 'none',
+	resize: 'none'
+}
  
 var Textarea = require('react-textarea-autosize');
 
 module.exports = React.createClass({
+	// componentDidMount: function() {
+ //      $(document.body).on('keyup', this.handleKeyUp);
+ //  }
+
+  // handleKeyUp: function(e) {
+  // 	console.log(e.keyCode);
+  // }
+
 	render: function () {
 		return (
 				<div>
-					<Textarea></Textarea>
-					<img src="img/691px-Gnu_meditate_levitate.png"></img>
+					<Textarea style={textareaStyle} autofocus>
+						Wshsooooo... describe your bug, young one
+					</Textarea>
+					<img style={imgStyle} src="img/691px-Gnu_meditate_levitate.png"></img>
 				</div>
 			)
 	}
